70add7eba296a6f9917ad4a2b9ced2d487ea41a9
Kevin J Holland <kevinholland94@gmail.com>
2026-01-12 12:33:07 -0500

    update fish config

--- config/config.fish
+++ config/config.fish
@@ -2,8 +2,27 @@
 #fish start- script |
 #----------------------
 
+
+set -gx UPLOAD_SSH "aster:/srv/http/astro/"
+set -gx UPLOAD_WEB "https://aster.host/astro/"
+set -gx UPLOAD_FOLDER "/Users/kevinholland/Desktop"
+
+set -gx VISUAL vim
+set -gx EDITOR $VISUAL
+
+set -gx PATH ~/.bin $PATH
+
 # Path to Oh My Fish install.
 set -gx OMF_PATH $HOME/.local/share/omf
+
+#jenv path
+set PATH $HOME/.jenv/bin $PATH
+
+ulimit -n 4096
+
+#this is broken
+#set -gx LESSOPEN "| /usr/local/bin/src-hilite-lesspipe.sh %s"
+#set -gx LESS " -R "
 
 # Customize Oh My Fish configuration path.
 set -gx OMF_CONFIG $HOME/.config/omf
@@ -11,14 +30,34 @@ set -gx OMF_CONFIG $HOME/.config/omf
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
 
-#add typesafe activator to path
-set -gx PATH ~/activator $PATH
-
 #set default bd option to case insensitive (-i) implies seems mode (-s)
 set -gx BD_OPT 'insensitive'
 
-function cl -d "Change to directory and do ls"
-    cd $argv; ls
+function halp -d "Alias for fuck"
+    fuck
+end
+
+function fucku -d "Alias for fuck -y"
+    fuck -y
+end
+
+# Make an empty Git commit to trigger a build
+function build_it
+    git commit --allow-empty -m "Trigger Build"
+end
+
+#rat alias
+function rat -d "Runs rat"
+    ~/go/bin/rat
+end
+#git/rat alias
+function gitv -d "Runs rat also"
+    rat
+end
+
+#mosh to asteria
+function aster
+	mosh bskcx@aster.host
 end
 
 #ip functions
@@ -48,8 +87,58 @@ function gacp -d "git add commit push"
     end
 end
 
-echo "Here is a fortune for the hard times: "
-echo ""
-cowsay (fortune)
-echo ""
+function gb -d "Prints ONLY current git branch"
+    git branch | grep "\*" | awk '{print $2}'
+end
+
+function gbc -d "Copies ONLY current git branch"
+    gb | pbcopy
+end
+
+#grep find in file
+function fif -d "Find in files"
+    grep -rni . -e "$argv" --exclude-dir='**/.*'
+end
+
+function l -d "Alias for less"
+    less "$argv"
+end
+
+function kbdoff -d "Turn keyboard off"
+    sudo kextunload /System/Library/Extensions/AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext/
+end
+
+function kbdon -d "Turn keyboard on"
+    sudo kextload /System/Library/Extensions/AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext/
+end
+
+function dui -d "Takes you to DUI source"
+    echo 'WARPING TO DUI SOURCE'
+    cd /Volumes/SourceCode/source/discoveryui
+end
+function ds -d "Takes you to DS source"
+    echo 'WARPING TO DS SOURCE'
+    cd /Volumes/SourceCode/source/discoveryservices
+end
+function dr -d "Takes you to DR source"
+    echo 'WARPING TO DR SOURCE'
+    cd /Volumes/SourceCode/source/deliveryresolver
+end
+
+#curl aliases
+function cxml -d "Curls with Accept: application/xml"
+    curl -H "Accept: application/xml" $argv
+end
+
+function cjson -d "Curls with Accept: application/json"
+    curl -H "Accept: application/json" $argv
+end
+
+set fisher_home ~/.local/share/fisherman
+set fisher_config ~/.config/fisherman
+source $fisher_home/config.fish
+
+source /usr/local/opt/asdf/libexec/asdf.fish
+
+set -gx PATH ~/.asdf/shims $PATH
 
